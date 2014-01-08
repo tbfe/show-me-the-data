691f0e72980d36145f104ce0435e50a8f0c27063
leeyeh <lee55962698@gmail.com>
2014-01-08 20:48:08 +0800

    redesign the icon.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: 'src',
-                    src: '**',
+                    src: ['**', '!**/*.psd'],
                     dest: 'output'
                 }]
             }
